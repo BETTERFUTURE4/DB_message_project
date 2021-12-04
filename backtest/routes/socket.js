8b01a17a6d439a70a4bf24e85db87d08a475ce49
Sumin Chung <87513424+finesum@users.noreply.github.com>
2021-12-04 22:44:53 +0900

    Update CHAT_MESSAGE
    일반 메시지 전송과 시간 랑데부 메시지 전송 기본적인 틀만 잡아 놨습니다.
    조금 무식한 방법인 것 같아 조금 더 좋은 방법이 있다면 수정 부탁 드립니다

--- backtest/routes/socket.js
+++ backtest/routes/socket.js
@@ -43,17 +43,46 @@ module.exports = io => {
 		socket.on('CHAT_GENERAL_MESSAGE', async msg => {
 			const targetSockets = findSocketById(io, msg.targetId);
 
-			await query(`INSERT INTO chat(SENDER, RECEIVER, TEXT, CREATED_AT) SELECT f.id, t.user_id, '${msg.message}', '${msg.created_at}' FROM users f, users t WHERE f.id = '${socket.user_id}' and t.id = '${msg.targetId}';`)
+			await query(`INSERT INTO chat(SENDER, RECEIVER, CREATED_AT, MSG_TYPE, TEXT) SELECT f.id, t.user_id, '${msg.created_at}', ${msg.msg_type}, '${msg.message}' FROM users f, users t WHERE f.id = '${socket.user_id}' and t.id = '${msg.targetId}';`)
 
 			if (targetSockets.length > 0) {
-				targetSockets.forEach(soc => soc.emit('CHAT_MESSAGE', {
+				targetSockets.forEach(soc => soc.emit('CHAT_GENERAL_MESSAGE', {
 					message: msg.message,
 					from_id: socket.user_id,
 					from_name: socket.name,
-					created_at: msg.created_at
+					created_at: msg.created_at,
+					msg_type: msg.msg_type
 				}));
 			}
 		});
+		
+	//SEND RENDEVOUS MESSAGE
+		socket.on('CHAT_RENDEVOUS_MESSAGE', async msg => {
+			const targetSockets = findSocketById(io, msg.targetId);
+
+			await query(`INSERT INTO chat(SENDER, RECEIVER, CREATED_AT, MSG_TYPE, TEXT, VALID_TIME) SELECT f.id, t.user_id, '${msg.created_at}', ${msg.msg_type}, '${msg.message}', ${msg.valid_time} FROM users f, users t WHERE f.id = '${socket.user_id}' and t.id = '${msg.targetId}'
+			AND EXISTS (SELECT BD_NAME, FLOOR_NUM, SSID FROM location WHERE BD_NAME = '${msg.user_bd}' AND BD_NAME = '${msg.target_bd}' AND FLOOR_NUM = ${msg.user_floor} AND FLOOR_NUM = ${msg.target_floor} AND SSID = '${msg.user_ssid}' AND SSID = '${msg.target_ssid}';) ;`);
+
+			if (targetSockets.length > 0) {
+				targetSockets.forEach(soc => soc.emit('CHAT_RENDEVOUS_MESSAGE', {
+					message: msg.message,
+					from_id: socket.user_id,
+					from_name: socket.name,
+					created_at: msg.created_at,
+					msg_type: msg.msg_type,
+					valid_time: msg.valid_time,
+					from_bd: msg.user_bd,
+					to_bd: msg.target_bd,
+					from_floor: msg.user_floor,
+					to_floor: msg.target_floor,
+					from_SSID: msg.user_ssid,
+					to_SSID: msg.target_ssid
+
+				}));
+			}
+		});
+			
+			
 
 		socket.on("disconnect", () => {0
 			if (socket.user_id) {
