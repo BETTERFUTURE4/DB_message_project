7e8bd1058d3e43d760628fdaf89a7c9257908121
Sumin Chung <87513424+finesum@users.noreply.github.com>
2021-12-07 23:38:52 +0900

    Update route_place.js
    쿼리 수정 완료 (작동 확인)

--- backtest/routes/route_place.js
+++ backtest/routes/route_place.js
@@ -9,7 +9,7 @@ const {query} = require('../modules/db');
 router.get('/place', async(req, res, next) => {
   try {
     const place_userID = req.body;
-    [rows, fields] = await query (`SELECT BD_NAME, FLOOR_NUM, SSID FROM location WHERE BD_NAME IN (SELECT BD_NAME FROM location WHERE USER_ID = '${place_userID}' AND LOCATION_STATE = 1;`);
+    [rows, fields] = await query (`SELECT BD_NAME, FLOOR_NUM, SSID FROM location WHERE BD_NAME IN (SELECT BD_NAME FROM location WHERE USER_ID = '${place_userID}' AND LOCATION_STATE = 1)`);
     
     res.send(rows);
     
@@ -26,7 +26,7 @@ router.get('/:SSID', async(req, res, next) => {
     [rows, fields] = await query (`SELECT NAME, TYPE, LOGIN_STATE, STATUS_MESSAGE
                                   FROM user WHERE ID IN (SELECT user_id FROM location
                                   WHERE SSID = '${this_ssid}' AND LOCATION_STATE = 1 OR LOCATION_STATE = 2
-                                  AND USER_ID != '${userID}';);`);
+                                  AND USER_ID != '${userID}')`);
 
     res.send(rows);
   }
@@ -43,11 +43,14 @@ router.get('/nearby', async(req, res, next) => {
     const near_userID = req.body;
 
     const { user_lat, user_long } = await query(`SELECT LATITUDE, LONGTITUDE FROM location
-                                                WHERE USER_ID = '${near_userID}' AND LOCATION_STATE = 1;`);
+                                                WHERE USER_ID = '${near_userID}' AND LOCATION_STATE = 1`);
 
-    const queryString = `SELECT A.NAME, A.TYPE, A.LOGIN_STATE, A.STATUS_MESSAGE,
-                        (6371*acos(cos(radians('${user_lat}'))*cos(radians(B.LATITUDE))*cos(radians(B.LONGTITUDE)-radians('${user_long}'))+sin(radians('${user_lat}'))*sin(radians(B.LATITUDE))))
-                        AS distance FROM user A, location B HAVING distance <= 0.5 ORDER BY distance WHERE B.USER_ID != '${near_userID}';`;
+    const queryString = `SELECT user.NAME, user.TYPE, user.LOGIN_STATE, user.STATUS_MESSAGE,
+                        (6371*acos(cos(radians('${user_lat}'))*cos(radians(locaton.LATITUDE))*cos(radians(location.LONGTITUDE)-radians('${user_long}'))+sin(radians('${user_lat}'))*sin(radians(location.LATITUDE))))
+                        AS distance FROM user A
+                        LEFT JOIN location ON user.ID = location.USER_ID
+                        WHERE location.USER_ID != '${near_userID}'
+                        HAVING distance <= 0.5 ORDER BY distance`;
     [rows, fields] = await query (queryString);
 
     res.send(rows);
@@ -66,11 +69,11 @@ router.post('/newplace', async(req, res, next) => {
     const {new_userID, new_lat, new_long, new_bd_name, new_f_num, new_ssid, new_ip} = req.body;
     
     const checkExist = await query (`SELECT EXISTS(SELECT * FROM location WHERE USER_ID = '${new_userID}' AND LATITUDE = '${new_lat}' AND LONGTITUDE = '${new_long}'
-                                      AND BD_NAME = '${new_bd_name}' AND FLOOR_NUM = ${new_f_num} AND SSID = '${new_ssid}' AND IP = '${new_ip}';);`);
+                                      AND BD_NAME = '${new_bd_name}' AND FLOOR_NUM = ${new_f_num} AND SSID = '${new_ssid}' AND IP = '${new_ip}'`);
 
     if (checkExist == 0) {
       await query (`INSERT INTO location(USER_ID, LOCATION_STATE, LATITUDE, LONGTITUDE, BD_NAME, FLOOR_NUM, SSID, IP)
-      VALUES('${new_bd_name}', 4, '${lat}', '${long}', '${bd_name}', ${f_num}, '${ssid}', '${ip}');`);
+      VALUES('${new_userID}', 4, '${new_lat}', '${new_long}', '${new_bd_name}', ${new_f_num}, '${new_ssid}', '${new_ip}')`);
     }
 
     else {
