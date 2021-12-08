5f346b6078b4c86f5bf4793778d45296762b9a65
Sumin Chung <87513424+finesum@users.noreply.github.com>
2021-12-09 02:16:22 +0900

    Final location.js

--- routes/location.js
+++ routes/location.js
@@ -1,18 +1,26 @@
 var express = require('express');
+const { response } = require('../app');
 var router = express.Router();
-router.use(express.json());
+//router.use(express.json());
 
 const {query} = require('../modules/db');
-
 
 /* Get place list */
 router.get('/place', async(req, res, next) => {
   try {
-    const place_userID = req.body;
-    [rows, fields] = await query (`SELECT BD_NAME, FLOOR_NUM, SSID FROM location WHERE BD_NAME IN (SELECT BD_NAME FROM location WHERE USER_ID = '${place_userID}' AND LOCATION_STATE = 1)`);
-    
-    res.send(rows);
-    
+    const {place_userID} = req.body;
+    const queryResult = await query(`SELECT BD_NAME, FLOOR_NUM, SSID FROM location WHERE USER_ID = '${place_userID}' AND LOCATION_STATE = 1;`);
+    if(queryResult.length > 0){
+      res.json({
+        success: true,
+        rows: queryResult
+      });
+    }
+    else{
+      res.json({
+        success: false
+      });
+    } 
   }
   catch(error){
     next(error);
@@ -23,12 +31,21 @@ router.get('/place', async(req, res, next) => {
 router.get('/SSID', async(req, res, next) => {
   try {
     const { userID, this_ssid } = req.body;
-    [rows, fields] = await query (`SELECT NAME, TYPE, LOGIN_STATE, STATUS_MESSAGE
+    const queryResult = await query (`SELECT NAME, TYPE, LOGIN_STATE, STATUS_MESSAGE
                                   FROM user WHERE ID IN (SELECT user_id FROM location
                                   WHERE SSID = '${this_ssid}' AND LOCATION_STATE = 1 OR LOCATION_STATE = 2
-                                  AND USER_ID != '${userID}')`);
-
-    res.send(rows);
+                                  AND USER_ID != '${userID}');`);
+    if(queryResult.length > 0){
+      res.json({
+        success: true,
+        rows: queryResult
+      });
+    }
+    else{
+      res.json({
+        success: false
+      });
+    }
   }
   catch(error) {
     next(error);
@@ -40,20 +57,30 @@ router.get('/SSID', async(req, res, next) => {
 //CHECK WHETHER queryString works well
 router.get('/nearby', async(req, res, next) => {
   try {
-    const near_userID = req.body;
+    const {near_userID} = req.body;
 
-    const { user_lat, user_long } = await query(`SELECT LATITUDE, LONGTITUDE FROM location
+    const [rows, field] = await query(`SELECT LATITUDE, LONGTITUDE FROM location
                                                 WHERE USER_ID = '${near_userID}' AND LOCATION_STATE = 1`);
-
+    console.log(rows.LATITUDE, rows.LONGTITUDE);
     const queryString = `SELECT user.NAME, user.TYPE, user.LOGIN_STATE, user.STATUS_MESSAGE,
-                        (6371*acos(cos(radians('${user_lat}'))*cos(radians(locaton.LATITUDE))*cos(radians(location.LONGTITUDE)-radians('${user_long}'))+sin(radians('${user_lat}'))*sin(radians(location.LATITUDE))))
-                        AS distance FROM user A
+                        (6371*acos(cos(radians('${rows.LATITUDE}'))*cos(radians(location.LATITUDE))*cos(radians(location.LONGTITUDE)-radians('${rows.LONGTITUDE}'))+sin(radians('${rows.LATITUDE}'))*sin(radians(location.LATITUDE))))
+                        AS distance FROM user
                         LEFT JOIN location ON user.ID = location.USER_ID
                         WHERE location.USER_ID != '${near_userID}'
                         HAVING distance <= 0.5 ORDER BY distance`;
-    [rows, fields] = await query (queryString);
-
-    res.send(rows);
+    const queryResult = await query (queryString);
+    console.log(queryResult)
+    if(queryResult.length > 0){
+      res.json({
+        success: true,
+        result: queryResult
+      });
+    }
+    else{
+      res.json({
+        success: false
+      })
+    }
   }
   catch(error) {
     next(error);
@@ -68,14 +95,15 @@ router.post('/newplace', async(req, res, next) => {
   try {
     const {new_userID, new_lat, new_long, new_bd_name, new_f_num, new_ssid, new_ip} = req.body;
     
-    const checkExist = await query (`SELECT EXISTS(SELECT * FROM location WHERE USER_ID = '${new_userID}' AND LATITUDE = '${new_lat}' AND LONGTITUDE = '${new_long}'
+    const checkExist = await query (`SELECT * FROM location WHERE USER_ID = '${new_userID}' AND LATITUDE = '${new_lat}' AND LONGTITUDE = '${new_long}'
                                       AND BD_NAME = '${new_bd_name}' AND FLOOR_NUM = ${new_f_num} AND SSID = '${new_ssid}' AND IP = '${new_ip}'`);
-
-    if (checkExist == 0) {
+    if (checkExist.length == 0) {
+      res.json({
+        success: true
+      });
       await query (`INSERT INTO location(USER_ID, LOCATION_STATE, LATITUDE, LONGTITUDE, BD_NAME, FLOOR_NUM, SSID, IP)
-      VALUES('${new_userID}', 4, '${new_lat}', '${new_long}', '${new_bd_name}', ${new_f_num}, '${new_ssid}', '${new_ip}')`);
+                    VALUES('${new_userID}', 4, '${new_lat}', '${new_long}', '${new_bd_name}', ${new_f_num}, '${new_ssid}', '${new_ip}')`);
     }
-
     else {
       res.json({
         success: false,
