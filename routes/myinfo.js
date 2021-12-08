84becc94d097ae9cf99a3c9cbc32e560799becfd
Sumin Chung <87513424+finesum@users.noreply.github.com>
2021-12-09 02:17:58 +0900

    Final myinfo.js

--- routes/myinfo.js
+++ routes/myinfo.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-router.use(express.json());
+//router.use(express.json());
 
 const { query } = require('../modules/db');
 const { verifyMiddleWare } = require ('../modules/jwt');
@@ -9,9 +9,9 @@ const { verifyMiddleWare } = require ('../modules/jwt');
 router.post('/userStMsg', async(req, res, next) => {
   try {
     const { My_id, statusMsg} = req.body;
-    const mstRegex = /^[ㄱ-ㅎ가-힣]{1,20}$/
-    
+    const mstRegex = /^[ㄱ-ㅎ가-힣]{1,20}$/    
     if (!mstRegex.test(statusMsg)) {
+      console.log("T")
       res.json({
         success: false,
         errorMessage: '1자 이상 20자 이상의 한글로만 입력해주세요.'
@@ -19,6 +19,9 @@ router.post('/userStMsg', async(req, res, next) => {
     }
     else {
       await query(`UPDATE user SET status_message = '${statusMsg}' WHERE id = '${My_id}'`);
+      res.json({
+        success: true
+      })
     }
   } catch (error) {
     next(error)
@@ -30,44 +33,42 @@ router.post('/userStMsg', async(req, res, next) => {
 Bc the row will be created only when the user pressed the 'UPDATE' button */
 /*Search through the table and get the user's previous location in status 1 and turn it into 0.*/
 
-
 router.post('/locInfo', async(req, res, next) => {
   try {
-    const {my_id_loc, lat, long, bd_name, f_num, ssid, ip} = req.body;
-    const queryCheck = await query (`SELECT EXISTS (SELECT * FROM location
-                                    WHERE USER_ID = '${my_id_loc}'AND LOCATION_STATE = 1 AND LATITUDE = '${lat} AND LONGTITUDE = '${long}' AND BD_NAME = '${bd_name}'
-                                    AND FLOOR_NUM = ${f_num} AND SSID = '${ssid}' AND IP = '${ip}')`);
-
-    if (queryCheck == 1) {
+    const {my_id_loc, lat, long, bd_name, f_num, ssid, ip} = req.body;    
+    //const queryCheck = await query (`SELECT EXISTS (SELECT * FROM location
+    //                                WHERE USER_ID = '${my_id_loc}'AND LOCATION_STATE = 1 AND LATITUDE = '${lat}' AND LONGTITUDE = '${long}' AND BD_NAME = '${bd_name}'
+    //                                AND FLOOR_NUM = ${f_num} AND SSID = '${ssid}' AND IP = '${ip}')`);
+    const queryCheck = await query (`SELECT * FROM location
+                                      WHERE USER_ID = '${my_id_loc}'AND LOCATION_STATE = 1 AND LATITUDE = '${lat}' AND LONGTITUDE = '${long}' AND BD_NAME = '${bd_name}'
+                                      AND FLOOR_NUM = ${f_num} AND SSID = '${ssid}' AND IP = '${ip}';`);    
+    if (queryCheck.length > 0) {
+      console.log("SAME")
       res.json({
         success: false,
         errorMessage: '이전과 같은 위치입니다.'
       });
     }
-
     else {
+      console.log("DIFF")
       await query (`UPDATE location SET LOCATION_STATE = 0 WHERE USER_ID = '${my_id_loc}' AND LOCATION_STATE = 1`);
-
-
       await query (`INSERT INTO location(USER_ID, LOCATION_STATE, LATITUDE, LONGTITUDE, BD_NAME, FLOOR_NUM, SSID, IP)
-      VALUES('${my_id_loc}', 1, '${lat}', '${long}', '${bd_name}', ${f_num}, '${ssid}', '${ip}')`);
-  
-    }
-
-   
+                    VALUES('${my_id_loc}', 1, '${lat}', '${long}', '${bd_name}', ${f_num}, '${ssid}', '${ip}')`);
+      res.json({
+        success: true
+      });
+    }   
   } catch (error) {
     next(error)
   }
 })
 
-/*LogOut + Location_State : 2 //YOU SHOULD SET LOCATION_STATE = 1 WHEN LOGIN */
+/*LogOut + Location_State : 2 //YOU SHOULD SET LOCATION_STATE = 1 WHEN LOGIN 
 router.post('/signOut', verifyMiddleWare, async(req, res, next) => {
-  const {id, name} = req.decoded;
+  const id = req.decoded;
 
   if (id) {
-
     await query(`UPDATE location SET LOCATION_STATE = 2 WHERE user_id = '${id}' AND LOCATION_STATE = 1`);
-
     res.clearCookie('token').json({
       success: true
     })
@@ -79,31 +80,19 @@ router.post('/signOut', verifyMiddleWare, async(req, res, next) => {
     })
   }
 });
-
+*/
 /*Delete Account*/
 router.post('/delAccount', async(req, res, next) => {
   try {
-    const {delId, delPW} = req.body;
-
-    await query(`SELECT * from user WHERE id = '${delId}' AND pw = '${delPW}'`);
-      if (!err) {
-        if (rows[0] != undefined) {
-          await query(`DELETE from user WHERE id = '${id}'`);
-            if (!err) {
-              res.send('탈퇴 완료');
-            }
-            else {
-              res.send('error: ' + err);
-            }
-        }
-        else {
-          res.send('No data');
-        }
-      }
-      else {
-        res.send('error: ' + err);
-      }
-
+    const {delId} = req.body;
+    const queryResult = await query(`SELECT * from user WHERE id = '${delId}';`);    
+    if (queryResult.length > 0) {
+      await query(`DELETE from user WHERE id = '${delId}'`);          
+      res.send('탈퇴 완료');          
+    }
+    else {
+      res.send('No data');
+    }
   } catch(error) {
     next(error)
   }
